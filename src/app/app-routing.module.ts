bd4ddc40b716ce0ac469c3f078366393e98e2279
cherdyy <maxim123456789101112@gmail.com>
2019-10-22 13:02:26 +0300

    add profile component, lazy load modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) },
+  { path: 'sign-in', loadChildren: () => import('./sign-in/sign-in.module').then(m => m.SignInModule) },
+  { path: ':nickname', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
