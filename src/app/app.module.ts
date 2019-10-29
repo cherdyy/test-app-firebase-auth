18794231c9c08ad3ab79932749b50e7c72def1b1
cherdyy <maxim123456789101112@gmail.com>
2019-10-29 14:55:48 +0200

    store dev tools, core module fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
+import { CoreModule } from '@core/core.module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '@environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     AppRoutingModule,
     CoreModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,9 @@ import { AuthService } from './services/auth.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserService } from './services/user.service';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '@environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { LayoutsModule } from "@core/layouts/layouts.module";
+import { LayoutsModule } from '@core/layouts/layouts.module';
 
 @NgModule({
   declarations: [],
@@ -16,7 +14,6 @@ import { LayoutsModule } from "@core/layouts/layouts.module";
     CommonModule,
     BrowserAnimationsModule,
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
     LayoutsModule
@@ -25,6 +22,14 @@ import { LayoutsModule } from "@core/layouts/layouts.module";
     AuthService,
     UserService
   ],
+  exports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    LayoutsModule
+  ]
 })
 
 export class CoreModule {
